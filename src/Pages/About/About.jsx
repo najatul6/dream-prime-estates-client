ebb4e720e7e12b9167a55f86790447c7d9ba3c05
najatul6 <mdnajatulislam.develop@gmail.com>
2023-11-26 06:07:13 +0600

    About section Add clock and contact section design complete

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,24 +2,58 @@ import Lottie from 'lottie-react';
 import Img1 from '../../assets/About/Focus.json'
 import Img2 from '../../assets/About/Tenacity.json'
 import Img3 from '../../assets/About/Transparency.json'
+import { useEffect, useState } from 'react';
+import Clock from 'react-clock';
+import 'react-clock/dist/Clock.css';
 
 const About = () => {
+    const [value, setValue] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => setValue(new Date()), 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    const worldClock = (timezone, title) => {
+        const formatted = value.toLocaleTimeString('en-US', {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            hour12: true,
+            timeZone: timezone,
+        });
+        return (
+            <div key={timezone} className="my-5 flex flex-col justify-center items-center space-y-5">
+                <p>{`${title}`}</p>
+                <Clock value={value} />
+                <p>{formatted}</p>
+            </div>
+        );
+    };
+
     return (
+
         <div className="max-w-[1440px] mx-auto">
-            <div className="flex flex-col md:flex-row px-5 py-10 justify-between items-center text-white">
-                <div className="md:w-1/2 md:p-5">
-                    <h2 className="text-4xl font-bold uppercase mb-5 md:mb-10">Our Mission</h2>
-                    <p className="text-xl font-bold">
-                        We are a premier real estate agency dedicated to transforming dreams into reality, with a distinct focus on delivering top-tier and prime real estate offerings
-                    </p>
-                </div>
-                <div className="md:w-1/2 md:p-5">
-                    <div className="mx-auto mask mask-hexagon border-white ">
-                        <img src="https://i.ibb.co/LpLygTX/owner.jpg" alt="" />
+            <div className="my-20">
+                <div className="shadow-2xl shadow-[#4C40F7] rounded-xl my-14">
+                    <div className="grid grid-cols-4 bg-[#00113B] rounded-xl shadow-[#FFF] shadow-inner text-white">
+                        <div>
+                            {worldClock('Asia/Dhaka', 'Bangladesh')}
+                        </div>
+                        <div>
+                            {worldClock('America/New_York', 'New York Time')}
+                        </div>
+                        <div>
+                            {worldClock('America/Los_Angeles', 'Los Angeles Time')}
+                        </div>
+                        <div>
+                            {worldClock('Europe/London', 'London Time')}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="mb-20">
                 <hr />
                 <h2 className="text-4xl uppercase text-white font-bold text-center my-5">Our Values</h2>
                 <hr />
@@ -63,9 +97,22 @@ const About = () => {
                             </p>
                         </div>
                     </div>
-
                 </div>
             </div>
+            <div className="flex flex-col md:flex-row px-5 justify-between items-center text-white">
+                <div className="md:w-1/2 md:p-5">
+                    <h2 className="text-4xl font-bold uppercase mb-5 md:mb-10">Our Mission</h2>
+                    <p className="text-xl font-bold">
+                        We are a premier real estate agency dedicated to transforming dreams into reality, with a distinct focus on delivering top-tier and prime real estate offerings
+                    </p>
+                </div>
+                <div className="md:w-1/2 md:p-5">
+                    <div className="mx-auto mask mask-hexagon border-white ">
+                        <img src="https://i.ibb.co/LpLygTX/owner.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,8 +1,44 @@
+import contactBG from '../../assets/contact/contactUs.svg'
 
 const Contact = () => {
     return (
-        <div>
-            
+        <div className="max-w-[1440px] mx-auto my-20">
+            <div>
+                <img className='w-full' src={contactBG} alt="" />
+            </div>
+            <div className="flex flex-col md:flex-row justify-between items-center my-10 gap-6 text-white">
+                <div className="lg:w-1/2">
+                    <h2 className="text-2xl font-bold my-5">Get in Touch with Our Divisions: </h2>
+                    <hr />
+                    <p>
+                        <p>Industrial: (880) 1773-827414</p>
+                        <p>Residential Real Estate: (880) 1938-322737</p>
+                        <p>Commercial Real Estate: (880) 1773-827414</p>
+                        <p>Fortis: (880) 1773-827414</p>
+                        <p>Richmond Auto Auction: (880) 1773-827414</p>
+                        <p>Car-Rac Online Exchange: (880) 1773-827414</p>
+                        <p>Accelerated Tax Sales: (880) 1773-827414</p>
+                        <p>Appraisals: (880) 1773-827414</p>
+                    </p>
+                </div>
+                <div className="divider bg-white w-1 h-52 my-auto rounded-xl hidden lg:block"></div>
+                <div className="lg:w-1/2 md:flex-1">
+                    <h2 className="text-2xl font-bold my-5">Welcome to Dream Prime! We look forward to hearing from you!</h2>
+                    <hr />
+                    <p>
+                        Whether you are looking for specific items to buy, or you have assets you d like to sell, you ve come to the right place. For more than, we helping people just like you buy and sell assets successfully and on their terms.
+                    </p>
+                    <p>
+                        Buying? Through national networks and contacts, we can help you find most any asset.
+                    </p>
+                    <p>
+                        Selling? We can appraise your assets and help you sell them at top dollar through traditional sales, accelerated sales, auctions, or consignments. We can also pay cash for assets.
+                    </p>
+                    <p>
+                        Please complete the form below, email us at info@dreamprimeestates.com or give us a call at 804-232-3300. Motleys. The Buying and Selling Marketplace!
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
